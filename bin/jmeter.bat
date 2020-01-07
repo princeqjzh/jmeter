00cc050871ea707e35814ba49b758d45f3cf9ae5
Ji Zhi Qian <jizhi.qian@jimubox.com>
2020-01-07 17:22:41 +0800

    License version

--- bin/jmeter.bat
+++ bin/jmeter.bat
@@ -3,11 +3,11 @@ rem
 rem Licensed to the Apache Software Foundation (ASF) under one or more
 rem contributor license agreements.  See the NOTICE file distributed with
 rem this work for additional information regarding copyright ownership.
-rem The ASF licenses this file to you under the Apache License, Version 3.0
+rem The ASF licenses this file to you under the Apache License, Version 2.0
 rem (the "License"); you may not use this file except in compliance with
 rem the License.  You may obtain a copy of the License at
 rem
-rem http://www.apache.org/licenses/LICENSE-1.0
+rem http://www.apache.org/licenses/LICENSE-2.0
 rem
 rem Unless required by applicable law or agreed to in writing, software
 rem distributed under the License is distributed on an "AS IS" BASIS,
